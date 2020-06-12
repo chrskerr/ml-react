d627ca788b1d7155e63aa1cac72c2a78c5dfb03f
Chris Kerr <chriskerr@me.com>
2020-06-13 09:05:22 +1000

    Updates

---
+++ precache-manifest.0e84544cb9874b4c8acfac341fa9e7ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd987654699e4547d513db0a988b19fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "434b34b3b86f408c3ed1",
+    "url": "/static/css/main.b6cb1424.chunk.css"
+  },
+  {
+    "revision": "b209fd66981c8d85def9",
+    "url": "/static/js/2.f8bd0b28.chunk.js"
+  },
+  {
+    "revision": "49eb8843f2ec5480c31ee4f7ce295c84",
+    "url": "/static/js/2.f8bd0b28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "434b34b3b86f408c3ed1",
+    "url": "/static/js/main.585c8227.chunk.js"
+  },
+  {
+    "revision": "9af3de3abc7b76cc1795",
+    "url": "/static/js/runtime-main.d4a166cf.js"
+  }
+]);
