a84db627866fefba8d8a943bc2f69a678d6bb1fe
Chris Kerr <chriskerr@me.com>
2020-06-13 21:18:57 +1000

    updated filters

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,5 @@
 
-import React, { memo } from "react";
+import React, { memo, useEffect, useState } from "react";
 import PropTypes from "proptypes";
 import { useSubscription } from "@apollo/react-hooks";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -10,20 +10,29 @@ import { ResponsiveContainer, ScatterChart, Scatter, XAxis, YAxis, CartesianGrid
 
 const SUBSCRIPTION = gql`
 subscription SubcribeVersions {
-	versions( limit: 1, order_by: { id: desc }) {
-		id
-		predictions ( where: { prediction: { _is_null: false }, actual: { _is_null: false }}) {
-			actual id time
-			instrument
-			prediction
-		}
-	}
+    predictions ( where: { prediction: { _is_null: false }, actual: { _is_null: false }}) {
+        actual id time
+        instrument
+        prediction
+        _version
+    }
 }`;
 
 export default function Home () {
 	const { data, loading } = useSubscription( SUBSCRIPTION );
+	const [ instrument, setInstrument ] = useState( false );
+	const [ version, setVersion ] = useState( false );
 
-	const predictions = _.get( data, "versions[0].predictions" );
+	const allPredictions = _.get( data, "predictions" );
+	const versionsPredictions = _.filter( allPredictions, [ "_version", version ]);
+
+	const versions = _.uniq( _.map( allPredictions, "_version" ));
+	useEffect(() => { if ( !version ) setVersion( Math.max( versions ));}, [ versions ]);
+
+	const instruments = _.uniq( _.map( versionsPredictions, "instrument" ));
+	useEffect(() => { if ( !instrument ) setInstrument( _.first( instruments ));}, [ instruments ]);
+    
+	const instrumentsVersionsPredictions = _.filter( versionsPredictions, [ "instrument", instrument ]);
 
 	return (
 		<div className="body">
@@ -32,16 +41,35 @@ export default function Home () {
 				<p>All predictions pulled for my model, visualised and analysed.</p>
 			</div>
 
+			<div className="options">
+				<div>
+					<p>Select Version:</p>
+					<select value={ version } onChange={ e => setVersion( e.target.value )}>
+						{ !_.isEmpty( versions ) && _.map( versions, version => <option key={ version } value={ version }>{ version }</option> ) }
+					</select>
+				</div>
+				<div>
+					<p>Select Instruments:</p>
+					<select value={ instrument } onChange={ e => setInstrument( e.target.value )}>
+						{ !_.isEmpty( instruments ) && _.map( instruments, instrument => <option key={ instrument } value={ instrument }>{ instrument }</option> ) }
+					</select>
+				</div>
+			</div>
+
 			{ loading && 
 				<div className="loader">
 					<FontAwesomeIcon icon={ faSpinner } spin size="3x" />
 				</div> 
 			}
 
-			{ !_.isEmpty( predictions ) && 
+			{ ( !loading && !_.isEmpty( instrumentsVersionsPredictions )) &&
 				<div className="chart">
-					<Chart data={ predictions } />
+					<Chart data={ instrumentsVersionsPredictions } />
 				</div> 
+			}
+
+			{ ( !loading && _.isEmpty( instrumentsVersionsPredictions )) &&
+				<p>Nothing to display - change a filter!</p>
 			}
 		</div>
 	);
@@ -60,13 +88,13 @@ const Chart = memo( function Chart ({ data }) {
 	
 	return (
 		<ResponsiveContainer>
-			<ScatterChart margin={{ top: 20, right: 20, bottom: 20, left: 20 }}>
+			<ScatterChart margin={{ top: 20, bottom: 20 }}>
 				<CartesianGrid />
 				<XAxis type="number" dataKey="actual" name="actual" domain={ domain } label={{ value: "Actual Change (cents)", position: "bottom", offset: 0 }} />
-				<YAxis type="number" dataKey="prediction" name="prediction" domain={ domain } label={{ value: "Predicted Change (cents)", position: "centerBottom", angle: -90, offset: 5 }}/>
+				<YAxis type="number" dataKey="prediction" name="prediction" domain={ domain } label={{ value: "Predicted Change (cents)", position: "centerBottom", angle: 90, offset: 5 }} orientation="right" />
 				<Tooltip cursor={{ strokeDasharray: "3 3" }} />
 				<Scatter name="A school" data={ graphData } fill="#f9bc60" />
-				<ReferenceLine x={ 0 } />
+				<ReferenceLine x="actual" y="prediction" />
 			</ScatterChart>
 		</ResponsiveContainer>
 	);
