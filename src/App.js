130ec6586a437f836ac3fae8781b02b70fe7be34
Chris Kerr <chriskerr@me.com>
2020-06-14 08:29:02 +1000

    updated icons and subscription style

--- src/App.js
+++ src/App.js
@@ -2,39 +2,12 @@
 import React from "react";
 import Home from "./Home";
 import { ApolloProvider } from "@apollo/react-hooks";
-import ApolloClient from "apollo-client";
-import { WebSocketLink } from "apollo-link-ws";
-import { HttpLink } from "apollo-link-http";
-import { split } from "apollo-link";
-import { getMainDefinition } from "apollo-utilities";
-import { InMemoryCache } from "apollo-cache-inmemory";
+import ApolloClient from "apollo-boost";
+
 
 export default function App () {
-
-	const httpLink = new HttpLink({
-		uri: "https://boiling-ridge-24261.herokuapp.com/v1/graphql",
-	});
-    
-	const wsLink = new WebSocketLink({
-		uri: "wss://boiling-ridge-24261.herokuapp.com/v1/graphql",
-		options: {
-			reconnect: true,
-			timeout: 30000,
-		},
-	});
-
-	const link = split(
-		({ query }) => {
-			const { kind, operation } = getMainDefinition( query );
-			return kind === "OperationDefinition" && operation === "subscription";
-		},
-		wsLink,
-		httpLink,
-	);
-
 	const client = new ApolloClient({
-		link,
-		cache: new InMemoryCache(),
+		uri: "https://boiling-ridge-24261.herokuapp.com/v1/graphql",
 	});
 
 	return (
--- src/Home.js
+++ src/Home.js
@@ -1,16 +1,22 @@
 
 import React, { memo, useEffect, useState } from "react";
 import PropTypes from "proptypes";
-import { useSubscription } from "@apollo/react-hooks";
+import { useQuery, useLazyQuery } from "@apollo/react-hooks";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 import _ from "lodash";
 import gql from "graphql-tag";
 import { ResponsiveContainer, ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, ReferenceLine } from "recharts";
 
-const SUBSCRIPTION = gql`
-subscription SubcribeVersions {
-    predictions ( where: { prediction: { _is_null: false }, actual: { _is_null: false }}) {
+const GET_VERSIONS = gql`
+query GetVersions {
+    versions { id }
+}
+`;
+
+const GET_PREDICTIONS = gql`
+query GetPredictions ( $version: Int! ) {
+    predictions ( where: { prediction: { _is_null: false }, actual: { _is_null: false }, _version: { _eq: $version }}) {
         actual id time
         instrument
         prediction
@@ -19,15 +25,18 @@ subscription SubcribeVersions {
 }`;
 
 export default function Home () {
-	const { data, loading } = useSubscription( SUBSCRIPTION );
 	const [ instrument, setInstrument ] = useState( false );
 	const [ version, setVersion ] = useState( false );
+    
+	const { data: versionsData } = useQuery( GET_VERSIONS );
+	const [ getPredictions, { data, loading }] = useLazyQuery( GET_PREDICTIONS );
+	useEffect(() => getPredictions({ variables: { version }}), [ version ]);
 
 	const allPredictions = _.get( data, "predictions" );
 	const versionsPredictions = _.filter( allPredictions, [ "_version", version ]);
 
-	const versions = _.uniq( _.map( allPredictions, "_version" ));
-	useEffect(() => { if ( !version ) setVersion( Math.max( versions ));}, [ versions ]);
+	const versions = _.map( _.get( versionsData, "versions" ), "id" );
+	useEffect(() => { if ( !version ) setVersion( Math.max( versions )); }, [ versions ]);
 
 	const instruments = _.uniq( _.map( versionsPredictions, "instrument" ));
 	useEffect(() => { if ( !instrument ) setInstrument( _.first( instruments ));}, [ instruments ]);
@@ -87,16 +96,19 @@ const Chart = memo( function Chart ({ data }) {
 	const domain = [ -1 * largestVal, largestVal ];
 	
 	return (
-		<ResponsiveContainer>
-			<ScatterChart margin={{ top: 20, bottom: 20 }}>
-				<CartesianGrid />
-				<XAxis type="number" dataKey="actual" name="actual" domain={ domain } label={{ value: "Actual Change (cents)", position: "bottom", offset: 0 }} />
-				<YAxis type="number" dataKey="prediction" name="prediction" domain={ domain } label={{ value: "Predicted Change (cents)", position: "centerBottom", angle: 90, offset: 5 }} orientation="right" />
-				<Tooltip cursor={{ strokeDasharray: "3 3" }} />
-				<Scatter name="A school" data={ graphData } fill="#f9bc60" />
-				<ReferenceLine x="actual" y="prediction" />
-			</ScatterChart>
-		</ResponsiveContainer>
+		<>
+			<ResponsiveContainer>
+				<ScatterChart margin={{ top: 20, bottom: 20 }}>
+					<CartesianGrid />
+					<XAxis type="number" dataKey="actual" name="actual" domain={ domain } label={{ value: "Actual Change (cents)", position: "bottom", offset: 0 }} />
+					<YAxis type="number" dataKey="prediction" name="prediction" domain={ domain } label={{ value: "Predicted Change (cents)", position: "centerBottom", angle: 90, offset: 5 }} orientation="right" />
+					<Tooltip cursor={{ strokeDasharray: "3 3" }} />
+					<Scatter name="A school" data={ graphData } fill="#f9bc60" />
+					<ReferenceLine x="actual" y="prediction" />
+				</ScatterChart>
+			</ResponsiveContainer>
+			<a href="http://recharts.org/en-US/api/ReferenceLine" target="_blank" rel="noreferrer noopener">http://recharts.org/en-US/api/ReferenceLine</a>
+		</>
 	);
 }, _.isEqual );
 Chart.proptypes = {
